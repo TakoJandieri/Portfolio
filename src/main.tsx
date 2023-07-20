40d55262f0c4434ec8b93969ae11c18f439ee482
TakoJandieri <takojandieri@gmail.com>
2023-07-20 17:45:20 +0200

    change main

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,7 @@
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-// import HomePage from "./Components/HomePage.tsx";
-// import reportWebVitals from "./reportWebVitals";
 import { HashRouter } from "react-router-dom";
-// import Test from "./Test/Test.tsx";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <HashRouter>
